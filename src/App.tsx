6b9584e1c9a2aa65a516807b9593fc749ac4ae80
kachiDeGreat <mail.ricx@gmail.com>
2025-12-18 19:08:12 +0100

    quiz has started

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ const App: React.FC = () => {
       )} */}
 
       {/* Main content */}
-      {/* <div className="app-content">
+      <div className="app-content">
         {currentView === "registration" && (
           <Registration
             onRegistrationComplete={handleRegistrationComplete}
@@ -92,9 +92,9 @@ const App: React.FC = () => {
             <CheckID />
           </div>
         )}
-      </div> */}
+      </div>
 
-      <MaintenancePage />
+      {/* <MaintenancePage /> */}
       {/* <ExportToPDFButton /> */}
 
       <Toaster
