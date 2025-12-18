93f60166b114ff496ff564e4dba084698528c6a0
kachiDeGreat <mail.ricx@gmail.com>
2025-12-18 19:04:35 +0100

    contact button hidden

--- src/component/underMaintance/MaintenancePage.tsx
+++ src/component/underMaintance/MaintenancePage.tsx
@@ -47,9 +47,9 @@ const MaintenancePage: React.FC = () => {
         </div>
 
         <div className="maintenance-contact">
-          <a href="https://wa.link/7fu1sh" className="contact-link">
+          {/* <a href="https://wa.link/7fu1sh" className="contact-link">
             Contact Support
-          </a>
+          </a> */}
           {/* <a href="mailto:mail.ricx@gmail.com" className="contact-link">
             Contact Support
           </a> */}
